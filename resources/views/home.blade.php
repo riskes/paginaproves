7f679be41c821c0b835d847d1268cee4efc808b7
riskes <arisquez@gmail.com>
2018-08-22 16:13:29 +0200

    arreglar rutes homt - calendari

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <title>App Name - @yield('title')</title>
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
         @include('home_components.menu')
--- resources/views/menus/calendari.blade.php
+++ resources/views/menus/calendari.blade.php
@@ -1,1 +1,15 @@
-hola
+@extends('home')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<header></header>
+@section('content')
+<section class='content'>
+    <body>
+        hola
+    </body>
+</section>
+    
+@endsection
+</html>
+
+
