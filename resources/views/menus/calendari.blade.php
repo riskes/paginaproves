c3981d5b6e79bca8751bb9ead98d18366d68640c
riskes <arisquez@gmail.com>
2018-08-24 14:29:29 +0200

    calendari apunt per programar

--- resources/views/menus/calendari.blade.php
+++ resources/views/menus/calendari.blade.php
@@ -1,25 +1,41 @@
 @extends('home')
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('bootstrap/bootstrap-year-calendar/css/bootstrap-year-calendar.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('bootstrap/bootstrap-year-calendar/css/bootstrap-year-calendar.min.css') }}" rel="stylesheet" type="text/css">
+    <head>
+        <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('bootstrap/bootstrap-year-calendar/css/bootstrap-year-calendar.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('bootstrap/bootstrap-year-calendar/css/bootstrap-year-calendar.min.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    @section('content')
+        <body>
+            <div id="calendar" data-provide="calendar"></div>
+        </body>
+    @endsection
+    <script type="text/javascript" src="{{ asset('jquery/js/jquery-3.3.1.min.js') }}" ></script>
+    <script type="text/javascript" src="{{ asset('bootstrap/js/bootstrap.min.js') }}" ></script>
+    <script type="text/javascript" src="{{ asset('bootstrap/bootstrap-year-calendar/js/bootstrap-year-calendar.min.js') }}" ></script>
+    <script>
+        $('#calendar').calendar({
+            clickDay: function(e) { 
+                console.log("funciona");
+            }
+        });    
+        function editEvent(event) {
 
-</head>
+        }
 
-@section('content')
-<section class='content'>
-    <body>
-        <div data-provide="calendar"></div>
+        function deleteEvent(event) {
         
-    </body>
-</section>    
-@endsection
-<script type="text/javascript" src="{{ asset('jquery/js/jquery-3.3.1.min.js') }}" ></script>
+        }
+        
+        function saveEvent() {
 
-<script type="text/javascript" src="{{ asset('bootstrap/js/bootstrap.min.js') }}" ></script>
-<script type="text/javascript" src="{{ asset('bootstrap/bootstrap-year-calendar/js/bootstrap-year-calendar.min.js') }}" ></script>
+        }
+
+        $(function() {
+            
+        });
+    </script>
 </html>
 
 
