854e64fc9b198df34834bb2082b8f3bf5881dcbe
riskes <arisquez@gmail.com>
2018-08-27 14:47:27 +0200

    calendari ja pots clicar dies(sense obrir modal)

--- resources/views/menus/calendari.blade.php
+++ resources/views/menus/calendari.blade.php
@@ -3,38 +3,59 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('bootstrap/datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet" type="text/css" >
         <link href="{{ asset('bootstrap/bootstrap-year-calendar/css/bootstrap-year-calendar.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('bootstrap/bootstrap-year-calendar/css/bootstrap-year-calendar.min.css') }}" rel="stylesheet" type="text/css">
     </head>
+
     @section('content')
-        <body>
-            <div id="calendar" data-provide="calendar"></div>
-        </body>
+       
+    <div id="calendar" data-provide="calendar"></div>
+        
     @endsection
     <script type="text/javascript" src="{{ asset('jquery/js/jquery-3.3.1.min.js') }}" ></script>
     <script type="text/javascript" src="{{ asset('bootstrap/js/bootstrap.min.js') }}" ></script>
+    <script type="text/javascript" src="{{ asset('bootstrap/datepicker/js/bootstrap-datepicker.min.js') }}" ></script>
     <script type="text/javascript" src="{{ asset('bootstrap/bootstrap-year-calendar/js/bootstrap-year-calendar.min.js') }}" ></script>
+    
     <script>
-        $('#calendar').calendar({
-            clickDay: function(e) { 
-                console.log("funciona");
+    function editEvent(event) {
+   console.log("hola");
+}
+
+function deleteEvent(event) {
+ 
+}
+
+function saveEvent() {
+  
+}
+
+$(function() {
+    var currentYear = new Date().getFullYear();
+
+    $('#calendar').calendar({ 
+        enableContextMenu: true,
+        enableRangeSelection: true,
+        contextMenuItems:[
+            {
+                text: 'Update',
+                click: editEvent
+            },
+            {
+                text: 'Delete',
+                click: deleteEvent
             }
-        });    
-        function editEvent(event) {
-
-        }
-
-        function deleteEvent(event) {
-        
-        }
-        
-        function saveEvent() {
-
-        }
-
-        $(function() {
-            
-        });
+        ],     
+        clickDay: function(e) {
+            editEvent(e);
+        }          
+    });
+    
+    $('#save-event').click(function() {
+        saveEvent();
+    });
+});
     </script>
 </html>
 
